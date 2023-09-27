4e66ac1f1e29d9fe9cdd98618a1b512d2209e390
flexi12.dauphin@gmail.com <flexi12.dauphin@gmail.com>
2023-09-28 01:10:56 +0200

    hexa

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-pub mod map;
+mod map;
 
 use comfy::*;
 
@@ -6,6 +6,13 @@ simple_game!("Text Example", update);
 
 fn update(_c: &mut EngineContext) {
 
-    draw_poly_z(vec2(0.0, 0.0), 6, 5.0, 0.0, BLUE, 0, TextureParams { blend_mode: BlendMode::None, shader: None });
-
+    let size_x = 8;
+    let size_y = 16;
+    let map = map::TileMap::new(size_x, size_y);
+    for x in 0..size_x {
+        for y in 0..size_y {        
+            let coord = map.get_real_coord(x, y);
+            draw_poly_z(vec2(coord.0, coord.1), 6, 0.8, 0.0, BLUE, 0, TextureParams { blend_mode: BlendMode::None, shader: None });
+        }
+    }
 }
--- src/map.rs
+++ src/map.rs
@@ -1,24 +1,50 @@
 #[derive(Clone)]
-struct HexagonTile {
-    q: u16,
-    r: u16,
+pub struct HexagonTile {
+    q: i16,
+    r: i16,
+    x: f32,
+    y: f32,
 }
 
 impl HexagonTile {
-    pub fn new(q: u16, r: u16) -> Self {
-        Self { q, r }
+    pub fn new(x_arr: i16, y_arr: i16, shift: f32) -> Self {
+        let q = x_arr;
+        let r = y_arr - x_arr/2;
+        let x = 3.0 * (x_arr as f32 + 0.5 * (y_arr%2) as f32) - shift;
+        let y = f32::sqrt(3.0) * y_arr as f32/2.0 - shift;
+        Self { q, r, x, y }
+    }
+    
+    pub fn new_empty() -> Self {
+        Self { q: 0, r: 0, x: 0.0, y: 0.0 }
+    }
+
+    pub fn set_coordinates(&mut self, x_arr: i16, y_arr: i16, x_shift: f32, y_shift: f32) {
+        self.q = x_arr;
+        self.r = y_arr - x_arr/2;
+        self.x = 0.9* 3.0 * (x_arr as f32 + 0.5 * (y_arr%2) as f32) - x_shift;
+        self.y = 0.9 * f32::sqrt(3.0) * y_arr as f32/2.0 - y_shift;
     }
 }
 
-struct TileMap {
+pub struct TileMap {
     map_array: Vec<Vec<HexagonTile>>,
-    size_x: u16,
-    size_y: u16,
+    size_x: i16,
+    size_y: i16,
 }
 
 impl TileMap {
-    pub fn new(size_x: u16, size_y: u16) -> Self {
-        let map_array = vec![vec![HexagonTile::new(0, 0); size_y as usize]; size_x as usize];
+    pub fn new(size_x: i16, size_y: i16) -> Self {
+        let mut map_array = vec![vec![HexagonTile::new_empty(); size_y as usize]; size_x as usize];
+        for x in 0..size_x {
+            for y in 0..size_y {
+                map_array[x as usize][y as usize].set_coordinates(x, y, size_x as f32, size_y as f32 /2.0);
+            }
+        }
         Self { map_array, size_x, size_y }
+    }
+
+    pub fn get_real_coord(&self, x: i16, y: i16) -> (f32, f32) {
+        (self.map_array[x as usize][y as usize].x, self.map_array[x as usize][y as usize].y)
     }
 }
