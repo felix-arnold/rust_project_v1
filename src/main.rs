5c87d00008a307f5f654c8a2208849dd9ba3cc8a
flexi12.dauphin@gmail.com <flexi12.dauphin@gmail.com>
2023-10-14 22:47:59 +0200

    save

--- src/main.rs
+++ src/main.rs
@@ -15,29 +15,30 @@ fn setup(c: &mut EngineContext) {
     );
     let mut camera = main_camera_mut();
     camera.zoom = 100.0;
+    camera.center = vec2(50.0, 25.0);
     c.lighting.ambient_light_intensity = 2.0;
 }
 
 fn update(_c: &mut EngineContext) {
 
-    let size_x = 20;
-    let size_y = 20;
-    let tile_size = 4.0;
+    let size_x = 100;
+    let size_y = 100;
+    let tile_size = 1.0;
     let map = map::TileMap::new(size_x, size_y, tile_size);
     
-    let perlin = Perlin::new(3);
-    let frequency = 10.0;
+    let perlin = Perlin::new(69);
+    let frequency = 0.1;
 
     for x in 0..size_x {
         for y in 0..size_y {        
             let coord = map.get_real_coord(x, y);
-            let val = perlin.get([coord.0 as f64 * frequency + 0.5, coord.1 as f64 * frequency + 0.5]);
-            let color = comfy::Color::new(val as f32 * 0.5 + 0.55, val as f32 * 0.5 + 0.55, val as f32 * 0.5 + 0.55, 1.0);
+            let val = perlin.get([coord.0 as f64 * frequency + 0.05, coord.1 as f64 * frequency + 0.05]);
+            let color = comfy::Color::new(val as f32 * 0.4 + 0.5, val as f32 * 0.4 + 0.5, val as f32 * 0.4 + 0.5, 1.0);
             draw_sprite(
                 texture_id("tile"),
-                vec2(coord.0 - 40.0, coord.1 - 20.0),
+                vec2(coord.0, coord.1),
                 color,
-                size_y as i32 - map.get_real_coord(x, y).1 as i32,
+                map.get_z(x, y),
                 splat(tile_size),
             );
         }
--- src/map.rs
+++ src/map.rs
@@ -4,26 +4,26 @@ pub struct HexagonTile {
     r: i16,
     x: f32,
     y: f32,
+    z: i32
 }
 
 impl HexagonTile {
     
     pub fn new_empty() -> Self {
-        Self { q: 0, r: 0, x: 0.0, y: 0.0 }
+        Self { q: 0, r: 0, x: 0.0, y: 0.0, z: 0 }
     }
 
-    pub fn set_coordinates(&mut self, x_arr: i16, y_arr: i16, size: f32) {
+    pub fn set_coordinates(&mut self, x_arr: i16, y_arr: i16, size: f32, size_y: i16) {
         self.q = y_arr;
         self.r = x_arr - y_arr/2;
         self.x = size * (x_arr as f32 + 0.5 * (y_arr%2) as f32);
         self.y = size * y_arr as f32 / 2.0;
+        self.z = (size_y - y_arr) as i32;
     }
 }
 
 pub struct TileMap {
-    map_array: Vec<Vec<HexagonTile>>,
-    size_x: i16,
-    size_y: i16,
+    map_array: Vec<Vec<HexagonTile>>
 }
 
 impl TileMap {
@@ -31,17 +31,17 @@ impl TileMap {
         let mut map_array = vec![vec![HexagonTile::new_empty(); size_y as usize]; size_x as usize];
         for x in 0..size_x {
             for y in 0..size_y {
-                map_array[x as usize][y as usize].set_coordinates(x, y, tile_size);
+                map_array[x as usize][y as usize].set_coordinates(x, y, tile_size, size_y);
             }
         }
-        Self { map_array, size_x, size_y }
+        Self { map_array }
     }
 
     pub fn get_real_coord(&self, x: i16, y: i16) -> (f32, f32) {
         (self.map_array[x as usize][y as usize].x, self.map_array[x as usize][y as usize].y)
     }
 
-    pub fn get_coord(&self, x: i16, y: i16) -> (i16, i16) {
-        (self.map_array[x as usize][y as usize].q, self.map_array[x as usize][y as usize].r)
+    pub fn get_z(&self, x: i16, y: i16) -> i32 {
+        self.map_array[x as usize][y as usize].z
     }
 }
